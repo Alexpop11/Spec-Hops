fee1363f5fddfd64f08974c9dbdd7cfdf14fcfb0
Alexpop11 <alexpopovitch@gmail.com>
2024-08-13 18:40:30 -0400

    Added arrow key support for movement

--- OpenGL/src/game_objects/Player.cpp
+++ OpenGL/src/game_objects/Player.cpp
@@ -11,19 +11,19 @@ Player::Player(const std::string& name, float x, float y)
 }
 
 void Player::update() {
-    if (Input::keys_pressed_down[GLFW_KEY_W])
+    if (Input::keys_pressed_down[GLFW_KEY_W] || Input::keys_pressed_down[GLFW_KEY_UP])
     {
         y += 1;
     }
-    if (Input::keys_pressed_down[GLFW_KEY_A])
+    if (Input::keys_pressed_down[GLFW_KEY_A] || Input::keys_pressed_down[GLFW_KEY_LEFT])
     {
         x -= 1;
     }
-    if (Input::keys_pressed_down[GLFW_KEY_S])
+    if (Input::keys_pressed_down[GLFW_KEY_S] || Input::keys_pressed_down[GLFW_KEY_DOWN])
     {
         y -= 1;
     }
-    if (Input::keys_pressed_down[GLFW_KEY_D])
+    if (Input::keys_pressed_down[GLFW_KEY_D] || Input::keys_pressed_down[GLFW_KEY_RIGHT])
     {
         x += 1;
     }
