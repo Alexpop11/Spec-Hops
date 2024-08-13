555a179b7da1a9bff4fc39f6287c3997c8846c8d
greg7mdp <greg7mdp@gmail.com>
2024-08-12 20:47:29 -0400

    Incomment include.

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -6,8 +6,8 @@
 #include <fstream>
 #include <string>
 #include <sstream>
-// #include <AL/al.h>
-// #include <AL/alc.h>
+#include <AL/al.h>
+#include <AL/alc.h>
 
 #define STB_IMAGE_IMPLEMENTATION // for icon
 #include "stb_image.h"
