734bb166615bd172f6656bcbffce2a107b278c95
Andre Popovitch <andre@popovit.ch>
2024-08-18 18:56:02 -0500

    make resources fallback relative to binary rather than run directory

--- OpenGL/src/Renderer.cpp
+++ OpenGL/src/Renderer.cpp
@@ -31,19 +31,26 @@ void Renderer::Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& 
 
 const std::string& Renderer::ResPath() {
    namespace fs = std::filesystem;
+
    if (res_path.empty()) {
-      if (fs::exists("./res/shaders"))
-         res_path = "./res/";
+      // Get the path of the executable
+      fs::path exe_path = fs::weakly_canonical(fs::path("/proc/self/exe"));
+      fs::path exe_dir  = exe_path.parent_path();
+
+      // Check for "res/shaders" relative to the executable's directory
+      if (fs::exists(exe_dir / "res" / "shaders"))
+         res_path = (exe_dir / "res").string() + "/";
       else if (fs::exists(RES_PATH "/res/shaders"))
          res_path = RES_PATH "/res/";
       else
          std::cout << "Resource directory not found\n";
    }
+
    return res_path;
 }
 
 std::tuple<int, int> Renderer::WindowSize() const {
    int width, height;
    glfwGetFramebufferSize(window, &width, &height);
-   return { width, height };
+   return {width, height};
 }
