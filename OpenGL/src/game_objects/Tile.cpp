e0091ce716dfb22bc34350cce08fb789af01c42d
Andre Popovitch <andre@popovit.ch>
2024-09-26 18:55:34 -0500

    don't create and destroy the wall texture all the time

--- OpenGL/src/game_objects/Tile.cpp
+++ OpenGL/src/game_objects/Tile.cpp
@@ -1,21 +1,21 @@
 #include "Tile.h"
 
 Tile::Tile(const std::string& name, bool wall, bool unbreakable, float x, float y)
-   : SquareObject(name, 2, x, y, "Textures/wall.png")
+   : SquareObject(name, 2, x, y, "Textures/alt-wall-bright.png")
    , wall(wall)
    , unbreakable(unbreakable) {
    wallTextureUnbreakable = Texture::create(Renderer::ResPath() + "Textures/alt-wall-unbreakable.png");
    wallTexture            = Texture::create(Renderer::ResPath() + "Textures/alt-wall-bright.png");
 
-    // floor textures array
+   // floor textures array
    std::vector<std::string> floorTextures = {"Textures/2-alt-floor.png", "Textures/2-alt-floor-2.png"};
 
-    //alternate floor textures i made. I'm going to leave them here for convenience, just comment out the above line and uncomment this one
-    //std::vector<std::string> floorTextures = {"Textures/alt-floor.png", "Textures/alt-floor-2.png",
-    //                                         "Textures/alt-floor-2.png", "Textures/alt-floor-3.png"};
+   // alternate floor textures i made. I'm going to leave them here for convenience, just comment out the above line and
+   // uncomment this one std::vector<std::string> floorTextures = {"Textures/alt-floor.png", "Textures/alt-floor-2.png",
+   //                                          "Textures/alt-floor-2.png", "Textures/alt-floor-3.png"};
 
    // randomly select the texture
-   floorTexture           = Texture::create(Renderer::ResPath() + floorTextures[rand() % floorTextures.size()]);
+   floorTexture = Texture::create(Renderer::ResPath() + floorTextures[rand() % floorTextures.size()]);
 
 
    setTexture();
