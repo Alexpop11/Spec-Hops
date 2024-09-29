eed127d56d01a1608ce6b8501617f1be3fa9640e
Andre Popovitch <andre@popovit.ch>
2024-09-28 23:40:18 -0500

    Union scene geometry

--- OpenGL/src/game_objects/Fog.cpp
+++ OpenGL/src/game_objects/Fog.cpp
@@ -3,19 +3,41 @@
 #include "Tile.h"
 #include <ranges>
 #include <vector>
+#include "clipper2/clipper.h"
+using namespace Clipper2Lib;
 
 Fog::Fog()
    : GameObject("Fog of War", 6, {0, 0}) {}
 
 void Fog::setUpShader(Renderer& renderer) {}
 
+PathsD findPolygonUnion(const std::vector<std::vector<glm::vec2>>& polygons) {
+   PathsD subject;
+   for (const auto& polygon : polygons) {
+      PathD path = MakePathD<double>({});
+      for (const auto& point : polygon) {
+         path.emplace_back(point.x, point.y);
+      }
+      subject.push_back(path);
+   }
+   auto solution = Union(subject, FillRule::NonZero);
+   return solution;
+}
+
+
 void Fog::render(Renderer& renderer) {
    std::vector<std::vector<glm::vec2>> allBounds;
    auto                                tiles = World::where<Tile>([&](const Tile& tile) { return true; });
    for (auto tile : tiles) {
       auto bounds = tile->getBounds();
-      for (size_t i = 0; i + 1 < bounds.size(); ++i) { // Manual sliding window
-         renderer.Line(bounds[i], bounds[i + 1], {1, 0, 0});
+      allBounds.push_back(bounds);
+   }
+   auto combined = findPolygonUnion(allBounds);
+   for (const auto& path : combined) {
+      for (size_t i = 0; i < path.size(); ++i) {
+         auto start = path[i];
+         auto end   = path[(i + 1) % path.size()];
+         renderer.Line({start.x, start.y}, {end.x, end.y}, {1, 0, 0});
       }
    }
 }
