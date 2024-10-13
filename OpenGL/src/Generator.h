f22da1100cb8478acd6c3954eeb2b537a13dc789
Andre Popovitch <andre@popovit.ch>
2024-10-12 19:45:15 -0500

    Add generator

---
+++ OpenGL/src/Generator.h
@@ -0,0 +1,76 @@
+// Generator.h
+#pragma once
+#include <coroutine>
+#include <optional>
+#include <memory>
+#include <variant>
+#include <exception>
+
+template <typename T = void>
+struct Generator {
+   struct promise_type;
+   using handle_type = std::coroutine_handle<promise_type>;
+
+   struct promise_type {
+      std::optional<T>                  current_value;
+      std::variant<std::monostate, int> wait_for_frames; // For advanced waiting
+
+      Generator get_return_object() { return Generator{handle_type::from_promise(*this)}; }
+
+      std::suspend_always initial_suspend() { return {}; }
+
+      std::suspend_always final_suspend() noexcept { return {}; }
+
+      std::suspend_always yield_value(T value) {
+         current_value = value;
+         return {};
+      }
+
+      std::suspend_always yield_value(int frames) { // For waiting frames
+         wait_for_frames = frames;
+         return {};
+      }
+
+      void return_void() {}
+
+      void unhandled_exception() { std::terminate(); }
+   };
+
+   handle_type coro;
+
+   Generator(handle_type h)
+      : coro(h) {}
+
+   Generator(const Generator&)            = delete;
+   Generator& operator=(const Generator&) = delete;
+
+   Generator(Generator&& other) noexcept
+      : coro(other.coro) {
+      other.coro = nullptr;
+   }
+
+   Generator& operator=(Generator&& other) noexcept {
+      if (this != &other) {
+         if (coro)
+            coro.destroy();
+         coro       = other.coro;
+         other.coro = nullptr;
+      }
+      return *this;
+   }
+
+   ~Generator() {
+      if (coro)
+         coro.destroy();
+   }
+
+   bool move_next() {
+      if (!coro.done())
+         coro.resume();
+      return !coro.done();
+   }
+
+   std::optional<T> current_value() const { return coro.promise().current_value; }
+
+   std::variant<std::monostate, int> wait_for_frames() const { return coro.promise().wait_for_frames; }
+};
--- OpenGL/src/game_objects/GameObject.h
+++ OpenGL/src/game_objects/GameObject.h
@@ -5,6 +5,7 @@
 #include <optional>
 #include <memory>
 #include "glm/glm.hpp"
+#include "../Generator.h"
 
 #include "../rendering/Renderer.h"
 
@@ -35,6 +36,13 @@ public:
    glm::vec2    position;
    float        rotation = 0;
    float        scale    = 1.0f;
+
+
+   // Add coroutine
+   void addCoroutine(Generator<> coroutine) { coroutines.emplace_back(std::move(coroutine)); }
+
+   // Store active coroutines
+   std::vector<Generator<>> coroutines;
 
 private:
    // Add any private members here if needed
